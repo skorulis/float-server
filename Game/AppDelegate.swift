2b6dbd65384cdd3a77f9e46a4a1932130accb61a
Alex Skorulis <skorulis@gmail.com>
2018-06-25 09:53:51 +1000

    Add main game app

---
+++ Game/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Game
+//
+//  Created by Alexander Skorulis on 25/6/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
