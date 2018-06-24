5066d9dd5ac983a5103c77da724ef973a2188594
Alex Skorulis <skorulis@gmail.com>
2018-06-24 18:27:37 +1000

    Remove TODO example code

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,17 +2,6 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
-    
     let loreController = LoreController()
     
     router.get("lore",String.parameter, use:loreController.getLore)
